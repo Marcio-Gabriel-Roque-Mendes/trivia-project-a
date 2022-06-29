40861b7ef05d6d404a7a80fca530d4e2bb96b0b2
Bruno Rodrigues <bruno.eheurod@gmail.com>
2022-06-29 16:48:24 -0300

    comecando req 10

---
+++ src/components/NextButton.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class NextButton extends Component {
+  render() {
+    return (
+      <div>
+        a
+      </div>
+    );
+  }
+}
+
+// NextButton.propTypes = {
+
+// };
+
+export default NextButton;

0fff8e0d6c2d4f0aaa843904431634bc334a8adb
Bruno Rodrigues <bruno.eheurod@gmail.com>
2022-07-01 20:21:11 -0300

    PROJETO FINALIZADO

--- src/tests/testeEnd.test.js
+++ src/tests/testeEnd.test.js
@@ -1,29 +1,29 @@
-// import React from "react";
-// import { screen, waitFor } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
-// import App from "../App";
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 
-// describe("", () => {
-//   jest.setTimeout(40000);
+describe('', () => {
+  const TOTAL = 40000;
+  jest.setTimeout(TOTAL);
 
-//   it("Verifica se o button esta desabilitado", async () => {
-//     const { history } = renderWithRouterAndRedux(<App />);
-//     const campoName = screen.getByTestId("input-player-name");
-//     userEvent.type(campoName, "Ada Love");
+  it('Verifica se o button esta desabilitado', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const campoName = screen.getByTestId('input-player-name');
+    userEvent.type(campoName, 'Ada Love');
 
-//     const campoEmailGravatar = screen.getByTestId("input-gravatar-email");
-//     userEvent.type(campoEmailGravatar, "adalove@email.com");
+    const campoEmailGravatar = screen.getByTestId('input-gravatar-email');
+    userEvent.type(campoEmailGravatar, 'adalove@email.com');
 
-//     const botaoPlay = await screen.findByRole("button", { name: /play/i });
-//     userEvent.click(botaoPlay);
+    const botaoPlay = await screen.findByRole('button', { name: /play/i });
+    userEvent.click(botaoPlay);
 
-//     await waitFor(() => expect(history.location.pathname).toBe("/game"));
+    await waitFor(() => expect(history.location.pathname).toBe('/game'), { timeout: 3000 });
 
-//     const resposta = await screen.findByTestId("correct-answer", undefined, {
-//       timeout: 32000,
-//     });
-//     expect(resposta).toBe(ex);
-//     expect(resposta).toBeDisabled();
-//   });
-// });
+    const resposta = await screen.findByTestId('correct-answer', undefined, {
+      timeout: 2000,
+    });
+    await waitFor(() => expect(resposta).toBeDisabled(), { timeout: 32000 });
+  });
+});

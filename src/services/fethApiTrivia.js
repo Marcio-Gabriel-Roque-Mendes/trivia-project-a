95d6f9d9233f48b6d92088771513c89356fd3874
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2023-01-09 17:57:36 -0300

    Feat: removendo a API geradora de token

--- src/services/fethApiTrivia.js
+++ src/services/fethApiTrivia.js
@@ -1,11 +1,3 @@
-const URL_TOKEN = 'https://opentdb.com/api_token.php?command=request';
-
-const getToken = async () => {
-  const data = await fetch(URL_TOKEN);
-  const dataJson = await data.json();
-  return dataJson.token;
-};
-
 const getQuestion = async (token) => {
   const URL_QUESTION = `https://opentdb.com/api.php?amount=5&token=${token}`;
   const data = await fetch(URL_QUESTION);
